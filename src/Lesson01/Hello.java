7a0594be3e30f55fa89dbdeaaa73cc34a2563d73
FrankHamp <valdemarengelund@gmail.com>
2026-01-28 17:19:36 +0100

    First Commit

---
+++ src/Lesson01/Hello.java
@@ -0,0 +1,9 @@
+package Lesson01;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hej Verden!");
+        System.out.println("Hvor er jeg?");
+    }
+}
